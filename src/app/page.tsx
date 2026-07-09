fa0b0da21fa1de4d962cacbb875dd34bdcc38bf8
Owona Nguini Mattéo Jordan <jordanowona@icloud.com>
2026-07-09 16:08:49 +0100

    design(home): flex-grow sur hero pour coller ticker+footer en bas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function PublicHomePage() {
       <div className="min-h-screen bg-white dark:bg-gray-900 flex flex-col transition-colors duration-200">
 
         {/* Main Search Area - Google Style */}
-        <main className="flex flex-col items-center justify-start pt-12 pb-6 px-4 sm:px-6">
+        <main className="flex-grow flex flex-col items-center justify-start pt-12 pb-6 px-4 sm:px-6">
 
           {/* Logo */}
           <div className="mb-8 text-center">
